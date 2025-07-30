6d163e7e7ed7df0aa79146f5751750b52fe8d6a9
mibotechcoder <mibotechcoder@gmail.com>
2025-07-30 18:10:27 +0200

    Update render.yaml & main.py

--- main.py
+++ main.py
@@ -47,4 +47,5 @@ def check_password(n_clicks, pwd):
 server = app.server
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8050)), debug=False)
