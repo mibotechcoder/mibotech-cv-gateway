0a0f3bb20c10aa296c14a90d4cbf20da31d6aca4
mibotechcoder <mibotechcoder@gmail.com>
2025-07-30 19:45:06 +0200

    Update main for terminal effects

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def check_password(n_clicks, pwd):
     Input("typewriter", "n_intervals")
 )
 def typewriter_effect(n):
-    if n <= len(MESSAGE) - 1:
+    if n < len(MESSAGE) - 1:
         return MESSAGE[:n+1], "", False
     else:
         return MESSAGE, dcc.Location(href=GPT_LINK), True
